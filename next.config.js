05e5f6151dbc874db804a308382f9f24711342a6
adityagaikwad888 <adityakg99@gmail.com>
2025-08-05 21:49:36 +0530

    1st commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+      },
+    ],
+    unoptimized: process.env.NODE_ENV === "development",
+  },
+};
